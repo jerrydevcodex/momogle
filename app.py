3fd9022230923d775935e39bc32641473b68c953
jerry <jerrynicework@gmail.com>
2024-09-02 15:51:46 +0900

    240902 Switch FastAPI

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import xgboost as xgb
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
-app = Flask(__name__)
+app = FastAPI()
 
-# 머신러닝 모델 및 레이블 인코더 불러오기
+templates = Jinja2Templates(directory="templates")
 model = xgb.XGBClassifier()
 model.load_model('best_xgb_model.json')
 label_encoder = LabelEncoder()
-label_encoder.classes_ = np.load('label_classes.npy',allow_pickle=True)
+label_encoder.classes_ = np.load('label_classes.npy', allow_pickle=True)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse('home.html', {"request": request})
 
-@app.route('/recommendation', methods=['GET', 'POST'])
-def recommendation():
-    if request.method == 'POST':
-        gender = request.form['gender']
-        age = request.form['age']
-        taste = request.form['taste']
-        season = request.form['season']
-        
-        # 입력 데이터를 모델에 맞게 전처리
-        input_data = process_input(gender, age, taste, season)
-        menu_prediction = predict_menu(input_data)
-        
-        return render_template('recommendation.html', menu=menu_prediction)
-    return render_template('recommendation.html')
+@app.post("/recommendation", response_class=HTMLResponse)
+async def recommendation(request: Request, 
+                         gender: str = Form(...), 
+                         age: str = Form(...), 
+                         taste: str = Form(...), 
+                         season: str = Form(...)):
+    
+    # 입력 데이터를 모델에 맞게 전처리
+    input_data = process_input(gender, age, taste, season)
+    menu_prediction = predict_menu(input_data)
+    
+    return templates.TemplateResponse('recommendation.html', {"request": request, "menu": menu_prediction})
 
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
+@app.get("/settings", response_class=HTMLResponse)
+async def settings(request: Request):
+    return templates.TemplateResponse('settings.html', {"request": request})
 
 def process_input(gender, age, taste, season):
     # 여기에 입력 데이터를 처리하는 코드를 작성합니다.
@@ -44,5 +44,3 @@ def predict_menu(input_data):
     predicted_label = label_encoder.inverse_transform(prediction)
     return predicted_label[0]
 
-if __name__ == '__main__':
-    app.run(debug=True)
